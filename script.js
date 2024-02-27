468339820e412e1e06efda067081ae6c2f6da10c
Sneakoz <79712322+Sneakoz@users.noreply.github.com>
2024-02-27 22:26:12 +0500

    add score and win divs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let loss = 0;
 
 function getComputerChoice() {
     let options = [
-        "ROCK", "PAPER", "SCISSORS"
+        "Rock", "Paper", "Scissors"
     ];
     let random = Math.floor(Math.random() * 3);
     return selection = options[random];
@@ -11,32 +11,31 @@ function getComputerChoice() {
 
 
 function playRound(player, comp) {
-    player = player.toUpperCase();
     if (player == comp) {
         return `You both chose ${player}. Its a draw`
-    } else if (player == "ROCK") {
-        if (comp == "PAPER") {
+    } else if (player == "Rock") {
+        if (comp == "Paper") {
             loss++
             return "You Lose! Paper beats Rock"
         } else {
             win++
             return "You Win! Rock beats Scissors"
         }
-    } else if (player == "PAPER") {
-        if (comp == "SCISSORS") {
+    } else if (player == "Paper") {
+        if (comp == "Scissors") {
             loss++
             return "You Lose! Scissors beat Paper"
         } else {
             win++
             return "You Win! Paper beats Rock"
         }
-    } else if (player == "SCISSORS") {
-        if (comp == "ROCK") {
+    } else if (player == "Scissors") {
+        if (comp == "Rock") {
             loss++
             return "You Lose! Rock beats Scissors"
         } else {
             win++
-            return "You Win! Scissors beat Pater"
+            return "You Win! Scissors beat Paper"
         }
     } else {
         return "Invalid Input! Only rock, paper or scissors allowed"
@@ -46,16 +45,30 @@ function playRound(player, comp) {
 const rockbtn = document.querySelector('#rock')
 const paperbtn = document.querySelector('#paper')
 const scissorsbtn = document.querySelector('#scissors')
-const resultdiv = document.querySelector('div')
+const resultdiv = document.querySelector('#resultdiv')
+const scorediv = document.querySelector('#scorediv')
 
-rockbtn.addEventListener('click', () => playGame('rock'))
-paperbtn.addEventListener('click', () => playGame('paper'))
-scissorsbtn.addEventListener('click', () => playGame('scissors'))
+rockbtn.addEventListener('click', () => playGame('Rock'))
+paperbtn.addEventListener('click', () => playGame('Paper'))
+scissorsbtn.addEventListener('click', () => playGame('Scissors'))
 
 function playGame(player) {
     let comp = getComputerChoice()
-    result = playRound(player, comp)
+    let result = playRound(player, comp)
+    let score = `${win}-${loss}`
+    let score2 = `${loss}-${win}`
     resultdiv.textContent = result
+    if (win == 5){
+        scorediv.textContent = `YOU WIN ${score}`
+        win = 0
+        loss = 0
+    } else if(loss ==5) {
+        scorediv.textContent = `THE COMPUTER WINS ${score2}`
+        win = 0
+        loss = 0
+    } else {
+        scorediv.textContent = score
+    }
     console.log(result)
     // if (win > loss) {
     //     console.log(`You won ${win}-${loss}`)
